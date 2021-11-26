209bbb3968747aa1cd64b22370d735549d2303f2
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2021-11-26 13:40:13 +0200

    kahden eri tuotteen jälkeen korin hinta oikein

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -22,4 +22,8 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(Tuote("maito", 2))
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
+    def test_kahden_eri_tuotteen_jalkeen_hinta_oikein(self):
+        self.kori.lisaa_tuote(Tuote("maito", 2))
+        self.kori.lisaa_tuote(Tuote("leipä", 3))
+        self.assertEqual(self.kori.hinta(), 5)
 
