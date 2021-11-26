c807de91f70e4efe9bdfb50fb3a3a809a91bae5a
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2021-11-26 13:32:06 +0200

    tavaroiden maara toimii oikein

---
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -0,0 +1,41 @@
+from tuote import Tuote
+from ostos import Ostos
+
+class Ostoskori:
+    def __init__(self):
+        self._ostokset = set()
+        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
+
+    def tavaroita_korissa(self):
+        total = 0
+        for ostos in self._ostokset:
+            total += ostos.lukumaara()
+        return total
+        # kertoo korissa olevien tavaroiden lukumäärän
+        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
+        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
+
+    def hinta(self):
+        return 0
+        # kertoo korissa olevien ostosten yhteenlasketun hinnan
+
+    def lisaa_tuote(self, lisattava: Tuote):
+        # lisää tuotteen
+        for ostos in self._ostokset:
+            if ostos.tuotteen_nimi() == lisattava.nimi:
+                ostos.muuta_lukumaaraa(1)
+                return
+        self._ostokset.add(Ostos(lisattava))
+
+    def poista_tuote(self, poistettava: Tuote):
+        # poistaa tuotteen
+        pass
+
+    def tyhjenna(self):
+        pass
+        # tyhjentää ostoskorin
+
+    def ostokset(self):
+        pass
+        # palauttaa listan jossa on korissa olevat ostos-oliot
+        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
---
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -0,0 +1,14 @@
+import unittest
+from ostoskori import Ostoskori
+from tuote import Tuote
+
+class TestOstoskori(unittest.TestCase):
+    def setUp(self):
+        self.kori = Ostoskori()
+
+    def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
+        self.assertEqual(self.kori.hinta(), 0)
+
+    def test_tavaroiden_maara_oikein(self):
+        self.kori.lisaa_tuote(Tuote("maito", 2))
+        self.assertEqual(self.kori.tavaroita_korissa(), 1)
